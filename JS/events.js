c43cd23673b8d554b2f2f6d73907d6e05e60a0e0
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-02-07 18:05:30 +0300

    events

---
+++ JS/events.js
@@ -0,0 +1,19 @@
+// function sayHello() {
+//     while 
+//     document.write("Hello World")
+//     }
+/* function over() {
+    document.write("mouse over")
+}
+
+function out() {
+    document.write("mouse out")
+}
+ */
+
+// let okButton = document.getElementById("ok");
+
+// okButton.onclick = "mesut";
+
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".")
